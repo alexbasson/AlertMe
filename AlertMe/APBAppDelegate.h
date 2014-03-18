d405bee742d5ba586d55ed44d913cccd91a1e097
Alex Basson <alexbasson@gmail.com>
2014-03-17 21:14:50 -0400

    Create AlertMe project

---
+++ AlertMe/APBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APBAppDelegate.h
+//  AlertMe
+//
+//  Created by Alex on 3/17/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlertMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlertMe
+//
+//  Created by Alex on 3/17/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APBAppDelegate class]));
+    }
+}
