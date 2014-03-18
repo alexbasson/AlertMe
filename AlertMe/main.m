b51062ac1e3e113bcc6da65bccf7f7b85ba43344
Alex Basson <alexbasson@gmail.com>
2014-03-17 22:22:46 -0400

    Rename and clean up default template files

--- AlertMe/main.m
+++ AlertMe/main.m
@@ -1,18 +1,10 @@
-//
-//  main.m
-//  AlertMe
-//
-//  Created by Alex on 3/17/14.
-//  Copyright (c) 2014 Alex. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-#import "APBAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APBAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
