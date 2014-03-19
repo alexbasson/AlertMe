fe094cee8d2fb24dad66db7711a6aace2dded1a5
Alex Basson <alexbasson@gmail.com>
2014-03-18 21:43:53 -0400

    Use standard UIAlertView

--- AlertMe/ViewController.m
+++ AlertMe/ViewController.m
@@ -1,9 +1,43 @@
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UIAlertViewDelegate>
+@property (nonatomic, weak, readwrite) IBOutlet UIButton *button;
+@property (nonatomic, weak, readwrite) IBOutlet UILabel *label;
 @end
 
 @implementation ViewController
+
+- (IBAction)buttonTapped:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert!"
+                                                        message:@"Tap a response"
+                                                       delegate:self
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:@"Choice 1", @"Choice 2", nil];
+    [alertView show];
+}
+
+//
+// Lots and lots of other code
+//
+
+#pragma mark - <UIAlertViewDelegate>
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    NSString *labelText;
+    switch (buttonIndex) {
+        case 0:
+            labelText = @"You tapped 'Cancel'";
+            break;
+        case 1:
+            labelText = @"You tapped 'Choice 1'";
+            break;
+        case 2:
+            labelText = @"You tapped 'Choice 2'";
+            break;
+        default:
+            break;
+    }
+    self.label.text = labelText;
+}
 
 @end
--- Specs/ViewControllerSpec.mm
+++ Specs/ViewControllerSpec.mm
@@ -1,4 +1,6 @@
 #import "ViewController.h"
+#import "UIControl+Spec.h"
+#import "UIAlertView+Spec.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -6,10 +8,64 @@ using namespace Cedar::Doubles;
 SPEC_BEGIN(ViewControllerSpec)
 
 describe(@"ViewController", ^{
-    __block ViewController *model;
+    __block ViewController *controller;
 
     beforeEach(^{
-
+        controller = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"ViewController"];
+        controller.view should_not be_nil;
+    });
+    
+    describe(@"tapping the button", ^{
+        beforeEach(^{
+            [controller.button tap];
+        });
+        
+        it(@"should show an alert view", ^{
+            UIAlertView *alertView = [UIAlertView currentAlertView];
+            alertView should_not be_nil;
+            alertView.title = @"Alert!";
+            alertView.message = @"Tap a response";
+        });
+    });
+    
+    describe(@"responding to the alert view", ^{
+        __block UIAlertView *alertView;
+        
+        beforeEach(^{
+            [controller.button tap];
+            alertView = [UIAlertView currentAlertView];
+            alertView should_not be_nil;
+        });
+        
+        context(@"tapping 'Cancel'", ^{
+            beforeEach(^{
+                [alertView dismissWithCancelButton];
+            });
+            
+            it(@"should update the label", ^{
+                controller.label.text should equal(@"You tapped 'Cancel'");
+            });
+        });
+        
+        context(@"tapping 'Choice 1'", ^{
+            beforeEach(^{
+                [alertView dismissWithClickedButtonIndex:1 animated:NO];
+            });
+            
+            it(@"should update the label", ^{
+                controller.label.text should equal(@"You tapped 'Choice 1'");
+            });
+        });
+        
+        context(@"tapping 'Choice 2'", ^{
+            beforeEach(^{
+                [alertView dismissWithClickedButtonIndex:2 animated:NO];
+            });
+            
+            it(@"should update the label", ^{
+                controller.label.text should equal(@"You tapped 'Choice 2'");
+            });
+        });
     });
 });
 
