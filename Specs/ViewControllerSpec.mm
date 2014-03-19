673303ebbd1aca092eeeeed20876f3ac8edf726f
Alex Basson <alexbasson@gmail.com>
2014-03-18 21:18:20 -0400

    Add PivotalCoreKit to project

---
+++ Specs/ViewControllerSpec.mm
@@ -0,0 +1,16 @@
+#import "ViewController.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(ViewControllerSpec)
+
+describe(@"ViewController", ^{
+    __block ViewController *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
